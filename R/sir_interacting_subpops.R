550c0048fc2acf11bb2b3246dad0e60ca863fe4d
thanhlongb <thlong.hcm@gmail.com>
2023-05-09 16:49:03 +0700

    minor code refactor

--- R/sir_interacting_subpops.R
+++ R/sir_interacting_subpops.R
@@ -5,7 +5,7 @@ ds <- function(state, parameters, i)
   with(as.list(c(state, parameters)), {
     sum_beta_i <- 0
     for (j in 1:k) {
-      sum_beta_i <- sum_beta_i + get(paste0("beta", (i-1)*k+j))*get(paste0("i", j))
+      sum_beta_i <- sum_beta_i + beta[i,j]*get(paste0("i", j))
     }
     -sum_beta_i*get(paste0("s", i)) + mu - mu*get(paste0("s", i))
   })
@@ -18,9 +18,9 @@ di <- function(state, parameters, i)
   with(as.list(c(state, parameters)), {
     sum_beta_i <- 0
     for (j in 1:k) {
-      sum_beta_i <- sum_beta_i + get(paste0("beta", (i-1)*k+j))*get(paste0("i", j))
+      sum_beta_i <- sum_beta_i + beta[i,j]*get(paste0("i", j))
     }
-    sum_beta_i*get(paste0("s", i)) - get(paste0("nu", i))*get(paste0("i", i)) - mu*get(paste0("i", i))
+    sum_beta_i*get(paste0("s", i)) - nu[i]*get(paste0("i", i)) - mu*get(paste0("i", i))
   })
 }
 
@@ -29,26 +29,27 @@ di <- function(state, parameters, i)
 dr <- function(state, parameters, i)
 {
   with(as.list(c(state, parameters, i)), {
-    get(paste0("nu", i))*get(paste0("i", i)) - mu*get(paste0("r", i))
+    nu[i]*get(paste0("i", i)) - mu*get(paste0("r", i))
   })
 }
 
 SIR_interacting_subpops <- function(t, state, parameters) {
   with(as.list(c(state, parameters)), {
-    s_out <- c()
-    i_out <- c()
-    r_out <- c()
+    s_states <- c()
+    i_states <- c()
+    r_states <- c()
 
     for (i in 1:k) {
-      s_out <- c(s_out, ds(state, parameters, i))
-      i_out <- c(i_out, di(state, parameters, i))
-      r_out <- c(r_out, dr(state, parameters, i))
+      s_states <- c(s_states, ds(state, parameters, i))
+      i_states <- c(i_states, di(state, parameters, i))
+      r_states <- c(r_states, dr(state, parameters, i))
     }
 
-    list(c(s_out, i_out, r_out))
+    list(c(s_states, i_states, r_states))
   })
 }
 
+k <- 2
 state <- c(
   s = c(0.8, 0.8),
   i = c(0.2, 0.2),
@@ -58,15 +59,15 @@ beta_matrix <- c(
   c(0.05, 0.00),
   c(0.00, 0.05)
 )
-beta <- matrix(beta_matrix, nrow=2, ncol=2, byrow=TRUE)
-parameters <- c(
-  beta = beta,
+parameters <- list(
+  beta = matrix(beta_matrix, nrow=k, ncol=k, byrow=TRUE),
   nu = c(1/30, 1/30),
   mu = 0.001,
-  k  = 2
+  k = k
 )
 times<-seq(0,10000,by=0.5)
 
 sir_interacting_subpops <- as.data.frame(ode(y=state,times=times,func=SIR_interacting_subpops,parms=parameters))
-head(sir_generic_model)
-summary(sir_generic_model)
+head(sir_interacting_subpops)
+summary(sir_interacting_subpops)
+
