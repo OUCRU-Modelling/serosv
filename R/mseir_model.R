fa307e43bbe1fffea6b9382c1f97cf993284b5cd
Bùi Thành Long <36789245+thanhlongb@users.noreply.github.com>
2023-05-12 14:34:05 +0700

    Add SIR models (#5)
    * add SIR models

---
+++ R/mseir_model.R
@@ -0,0 +1,45 @@
+#' MSEIR model
+#'
+#' Refers to section 3.4.
+#'
+#' @param a age sequence
+#' @param gamma time in maternal class.
+#' @param lambda time in susceptible class.
+#' @param sigma time in latent class.
+#' @param nu time in infected class.
+#'
+#' @examples
+#' model <- mseir_model(
+#'   a=seq(from=1,to=20,length=500), # age range from 0 -> 20 yo
+#'   gamma=1/0.5, # 6 months in the maternal antibodies
+#'   lambda=0.2,  # 5 years in the susceptible class
+#'   sigma=26.07, # 14 days in the latent class
+#'   nu=36.5      # 10 days in the infected class
+#' )
+#' model
+#'
+#' @export
+mseir_model <- function(a, gamma, lambda, sigma, nu)
+{
+  ma  <- exp(-gamma*a)
+  sa  <- (gamma/(gamma-lambda))*(exp(-lambda*a)-exp(-gamma*a))
+  ea  <- ((lambda*gamma)/(gamma-lambda))*
+    (
+      ((exp(-sigma*a)-exp(-lambda*a))/(lambda-sigma))
+      -((exp(-sigma*a)-exp(-gamma*a))/(gamma-sigma))
+    )
+  ia  <- (sigma*lambda*gamma)*
+    (
+      ((exp(-nu*a)-exp(-sigma*a))/((lambda-sigma)*(gamma-sigma)*(sigma-nu)))
+      +((exp(-nu*a)-exp(-lambda*a))/((lambda-gamma)*(lambda-sigma)*(lambda-nu)))
+      +((exp(-nu*a)-exp(-gamma*a))/((gamma-lambda)*(gamma-sigma)*(gamma-nu)))
+    )
+  data.frame(
+    a =  c(0, a),
+    ma = c(1, ma),
+    sa = c(0, sa),
+    ea = c(0, ea),
+    ia = c(0, ia),
+    ra = c(0, ma - sa - ea - ia)
+  )
+}
---
+++ R/sir_basic_model.R
@@ -0,0 +1,66 @@
+# install.packages("deSolve")
+require(deSolve)
+
+sir_basic <- function(t, state, parameters)
+{
+  with(as.list(c(state, parameters)), {
+     N  <- S + I + R   # total population
+     lambda <- beta*I  # force of infection
+     dS <- N*mu*(1-p) - (lambda + mu)*S
+     dI <- lambda*S - (nu+alpha+mu)*I
+     dR <- N*mu*p + nu*I - mu*R
+     list(c(dS, dI, dR))
+  })
+}
+
+#' Basic SIR model
+#'
+#' Refers to section 3.1.3.
+#'
+#' @details
+#' In \code{state}:
+#'
+#'  - \code{S}: number of susceptible
+#'
+#'  - \code{I}: number of infected
+#'
+#'  - \code{R}: number of recovered
+#'
+#' In \code{parameters}:
+#'
+#'  - \code{alpha}: disease-related death rate
+#'
+#'  - \code{mu}: natural death rate (= 1/life expectancy)
+#'
+#'  - \code{beta}: transmission rate
+#'
+#'  - \code{nu}: recovery rate
+#'
+#'  - \code{p}: percent of population vaccinated at birth
+#'
+#' @param times time sequence.
+#'
+#' @param state the initial state of the model.
+#'
+#' @param parameters the parameters of the model.
+#'
+#' @examples
+#' state <- c(S=4999, I=1, R=0)
+#' parameters <- c(
+#'   mu=1/75, # 1 divided by life expectancy (75 years old)
+#'   alpha=0, # no disease-related death
+#'   beta=0.0005, # transmission rate
+#'   nu=1, # 1 year for infected to recover
+#'   p=0 # no vaccination at birth
+#' )
+#' times <- seq(0, 250, by=0.1)
+#' model <- sir_basic_model(times, state, parameters)
+#' model
+#'
+#' @importFrom deSolve ode
+#'
+#' @export
+sir_basic_model <- function(times, state, parameters)
+{
+  as.data.frame(ode(y=state,times=times,func=sir_basic,parms=parameters))
+}
