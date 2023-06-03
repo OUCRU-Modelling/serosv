e94a6ae2436b2dcc2e9ec3dc16372d4106340526
Bùi Thành Long <36789245+thanhlongb@users.noreply.github.com>
2023-06-03 14:38:10 +0700

    Add parametric models (#6)
    * add polynomial models 
    
    * fraction polynomial search function
    
    * nonlinear models
    
    * add unit tests

---
+++ R/polynomial_models.R
@@ -0,0 +1,151 @@
+X <- function(t, degree) {
+  X_matrix <- matrix(rep(1, length(t)), ncol = 1)
+  if (degree > 1) {
+    for (i in 2:degree) {
+      X_matrix <- cbind(X_matrix, i * t^(i-1))
+    }
+  }
+  -X_matrix
+}
+
+predictor <- function(degree) {
+  formula <- "cbind(tot-pos, pos)~-1"
+  for (i in 1:degree) {
+    formula <- paste0(formula, "+I(age^", i, ")")
+  }
+  formula
+}
+
+#' A polynomial model.
+#'
+#' Refers to section 6.1.1.
+#'
+#' @param age the age vector.
+#' @param pos the pos vector.
+#' @param tot the tot vector.
+#' @param deg the degree of the model.
+#'
+#' @examples
+#' df <- hav_bg_1964
+#' model <- polynomial_model(
+#'   df$age, df$pos, df$tot,
+#'   deg=2
+#'   )
+#' plot(model)
+#'
+#' @export
+polynomial_model <- function(age, pos, tot, deg=1) {
+  model <- list()
+
+  f <- predictor(deg)
+  model$info <- glm(
+    as.formula(f),
+    family=binomial(link="log")
+  )
+  X <- X(age, deg)
+  model$sp <- 1 - model$info$fitted.values
+  model$foi <- X%*%model$info$coefficients
+  model$df <- list(age=age, pos=pos, tot=tot)
+
+  class(model) <- "polynomial_model"
+  model
+}
+
+#' plot() overloading for polynomial model
+#'
+#' @param x the polynomial model object.
+#' @param ... arbitrary params.
+#'
+#' @export
+plot.polynomial_model <- function(x, ...) {
+  CEX_SCALER <- 4 # arbitrary number for better visual
+
+  with(x$df, {
+    par(las=1,cex.axis=1,cex.lab=1,lwd=2,mgp=c(2, 0.5, 0),mar=c(4,4,4,3))
+    plot(
+      age,
+      pos/tot,
+      cex=CEX_SCALER*tot/max(tot),
+      xlab="age", ylab="seroprevalence",
+      xlim=c(0, max(age)), ylim=c(0,1)
+    )
+    lines(age, x$sp, lwd=2)
+    lines(age, x$foi, lwd=2, lty=2)
+    axis(side=4, at=round(seq(0.0, max(x$foi), length.out=3), 2))
+    mtext(side=4, "force of infection", las=3, line=2)
+  })
+}
+
+#' The Farrington (1990) model.
+#'
+#' Refers to section 6.1.2.
+#'
+#' @param age the age vector.
+#' @param pos the pos vector.
+#' @param tot the tot vector.
+#' @param start Named list of vectors or single vector.
+#' Initial values for optimizer.
+#' @param fixed Named list of vectors or single vector.
+#' Parameter values to keep fixed during optimization.
+#'
+#' @examples
+#' df <- rubella_uk_1986_1987
+#' model <- farrington_model(
+#'   df$age, df$pos, df$tot,
+#'   start=list(alpha=0.07,beta=0.1,gamma=0.03)
+#'   )
+#' plot(model)
+#'
+#' @importFrom stats4 mle
+#'
+#' @export
+farrington_model <- function(age, pos, tot, start, fixed=list())
+{
+  farrington <- function(alpha,beta,gamma) {
+    p=1-exp((alpha/beta)*age*exp(-beta*age)
+            +(1/beta)*((alpha/beta)-gamma)*(exp(-beta*age)-1)-gamma*age)
+    ll=pos*log(p)+(tot-pos)*log(1-p)
+    return(-sum(ll))
+  }
+
+  model <- list()
+
+  model$info <- mle(farrington, fixed=fixed, start=start)
+  alpha <- model$info@coef[1]
+  beta  <- model$info@coef[2]
+  gamma <- model$info@coef[3]
+  model$sp <- 1-exp(
+    (alpha/beta)*age*exp(-beta*age)
+    +(1/beta)*((alpha/beta)-gamma)*(exp(-beta*age)-1)
+    -gamma*age)
+  model$foi <- (alpha*age-gamma)*exp(-beta*age)+gamma
+  model$df <- list(age=age, pos=pos, tot=tot)
+
+  class(model) <- "farrington_model"
+  model
+}
+
+#' plot() overloading for Farrington model
+#'
+#' @param x the Farrington model object.
+#' @param ... arbitrary params.
+#'
+#' @export
+plot.farrington_model <- function(x, ...) {
+  CEX_SCALER <- 4 # arbitrary number for better visual
+
+  with(x$df, {
+    par(las=1,cex.axis=1,cex.lab=1,lwd=2,mgp=c(2, 0.5, 0),mar=c(4,4,4,3))
+    plot(
+      age,
+      pos/tot,
+      cex=CEX_SCALER*tot/max(tot),
+      xlab="age", ylab="seroprevalence",
+      xlim=c(0, max(age)), ylim=c(0,1)
+    )
+    lines(age, x$sp, lwd=2)
+    lines(age, x$foi, lwd=2, lty=2)
+    axis(side=4, at=round(seq(0.0, max(x$foi), length.out=3), 2))
+    mtext(side=4, "force of infection", las=3, line=2)
+  })
+}
---
+++ R/weibull_model.R
@@ -0,0 +1,61 @@
+#' The Weibull model.
+#'
+#' Refers to section 6.1.2.
+#'
+#' @param t the time vector.
+#' @param spos the seropositive vector.
+#'
+#' @examples
+#' df <- hcv_be_2006[order(hcv_be_2006$dur), ]
+#' model <- weibull_model(
+#'   t=df$dur,
+#'   spos=df$seropositive
+#'   )
+#' plot(model)
+#'
+#' @export
+weibull_model <- function(t, spos)
+{
+  model <- list()
+
+  model$info <- glm(
+    spos~log(t),
+    family=binomial(link="cloglog")
+    )
+  b0 <- coef(model$info)[1]
+  b1 <- coef(model$info)[2]
+  model$foi <- exp(b0)*b1*exp(log(t))^(b1-1)
+  model$sp <- 1-exp(-exp(b0)*t^b1)
+  model$df <- data.frame(t=t, spos=spos)
+
+  class(model) <- "weibull_model"
+  model
+}
+
+#' plot() overloading for Weibull model
+#'
+#' @param x the Weibull model object.
+#' @param ... arbitrary params.
+#'
+#' @export
+plot.weibull_model <- function(x, ...) {
+  CEX_SCALER <- 4 # arbitrary number for better visual
+
+  df_ <- transform(x$df$t, x$df$spos)
+  names(df_)[names(df_) == "t"] <- "exposure"
+
+  with(c(x$df, df_), {
+    par(las=1,cex.axis=1,cex.lab=1,lwd=2,mgp=c(2, 0.5, 0),mar=c(4,4,4,3))
+    plot(
+      exposure,
+      pos/tot,
+      cex=CEX_SCALER*tot/max(tot),
+      xlab="exposure", ylab="seroprevalence",
+      xlim=c(0, max(exposure)), ylim=c(0,1)
+    )
+    lines(t, model$sp, lwd=2)
+    lines(t, model$foi, lwd=2, lty=2)
+    axis(side=4, at=round(seq(0.0, max(model$foi), length.out=10), 2))
+    mtext(side=4, "force of infection", las=3, line=2)
+  })
+}
